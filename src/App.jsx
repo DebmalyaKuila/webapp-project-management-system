8f177799bae48e8ba7a7710adaafca5b91221ef5
Debmalya Kuila <debmalyakuila2002@gmail.com>
2024-07-14 13:03:32 +0530

    get new accesstoken before it expires

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React, {useEffect }  from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
@@ -12,17 +12,35 @@ import Projects from './pages/Projects.jsx';
 import Clients from './pages/Clients.jsx';
 import Invoices from "./pages/Invoices.jsx"
 
+import axios from 'axios'
+
 function App() {
+
+   //get a new accessToken before it expires , maybe 40 min. as accesstoken expires in 45 minuites
+   useEffect(()=>{
+    const minutes=1000*60;
+    setInterval(() => {
+     const token=JSON.parse(localStorage.getItem("project-mangement-system"))?.refreshJWT
+     if(token){
+       axios.get(`${import.meta.env.VITE_API_BASE_URL}/v1/tokens/`, 
+         { headers: { Authorization: `Bearer ${token}` }})
+         .then(res =>{
+            sessionStorage.setItem("accessJWT",res.data.newAccessToken)
+         })
+     }
+    }, minutes * 40); 
+ 
+   },[])
 
   return (
     <Router>
             <Routes>
                 <Route exact path='/' element={<Login />} />
                 <Route path='/reset-password' element={<PasswordReset />} />
-                <Route path='/dashboard' element={<DefaultLayout><Dashboard/></DefaultLayout>} />
+                <Route path='/dashboard' element={<DefaultLayout ><Dashboard/></DefaultLayout>} />
                 <Route path='/employees' element={<DefaultLayout><Employees/></DefaultLayout>} />
-                <Route path='/projects' element={<DefaultLayout><Projects/></DefaultLayout>} />
-                <Route path='/clients' element={<DefaultLayout><Clients/></DefaultLayout>} />
+                <Route path='/projects' element={<DefaultLayout ><Projects/></DefaultLayout>} />
+                <Route path='/clients' element={<DefaultLayout ><Clients/></DefaultLayout>} />
                 <Route path='/invoices' element={<DefaultLayout><Invoices/></DefaultLayout>} />
             </Routes>
     </Router>
